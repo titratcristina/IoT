35732e26d0a63b4b744f4d25c5ad1eb84434e85b
Titrat Cristina <31698958+titratcristina@users.noreply.github.com>
2018-04-08 00:10:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+var gg1 = new JustGage({
+    id: 'gage1',
+    title: 'value',
+    min: 0,
+    max: 100,
+    counter: true,
+    hideInnerShadow: true,
+    value: 0,
+    levelColors: [
+                '#FFFF00',
+                '#00ff00',
+                '#1200ff'
+            ],
+    customSelctors: [{
+        color: '#FFFF00',
+        lo: 0,
+        hi: 20
+            }, {
+        color: '#00ff00',
+        lo: 21,
+        hi: 60
+            }, {
+        color: '#1200ff',
+        lo: 61,
+        hi: 100
+            }]
+});
+
+function update() {
+    console.log('se actualizeaza...');
+    $('#val').removeClass('alert-danger');
+    $('#val').removeClass('alert-success');
+    $('#val').addClass('alert-warning');
+    $('#val').text('Actualizare...');
+
+    $.get(location.protocol+'//'+location.host+'/value', function (data) {
+        gg1.refresh(data);
+    }).done(function () {
+        $('#val').removeClass('alert-warning');
+        $('#val').removeClass('alert-danger');
+        $('#val').addClass('alert-success');
+        $('#val').text('Actualizat cu succes');
+        console.log('OK');
+    }).fail(function () {
+        $('#val').removeClass('alert-success');
+        $('#val').removeClass('alert-warning');
+        $('#val').addClass('alert-danger');
+        $('#val').text('Eroare');
+        console.log('ERROR');
+    }).always(function () {});
+    
+};
+update();
+setInterval(function () {
+    update();
+}, 20000);
